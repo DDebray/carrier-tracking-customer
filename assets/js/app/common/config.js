b8afd50b3e0cd71baeab8dfd7b67b6a07820c172
Erik Panzer <erik.panzer@coureon.com>
2016-05-03 14:41:11 +0200

    configured environments

--- assets/js/app/common/config.js
+++ assets/js/app/common/config.js
@@ -16,24 +16,27 @@ module.exports = ['$translateProvider', function ($translateProvider) {
     ui : {
       development : 'https://api-testing.coureon.com/ui/',
       testing : 'https://api-testing.coureon.com/ui/',
+      staging : 'https://api-staging.coureon.com/ui/',
       production : 'https://api.coureon.com/ui/'
     },
     www : {
       development: 'http://localhost:3000',
       testing : 'https://www-testing.coureon.com',
+      staging : 'https://api-staging.coureon.com',
       production : 'https://www.coureon.com'
     },
     app : {
       development: 'http://localhost:3000/login',
       testing : 'https://app-testing.coureon.com/login',
+      staging : 'https://api-staging.coureon.com/login/',
       production : 'https://app.coureon.com/login'
     }
   };
 
   self.endpoints.cms = {
     development : self.endpoints.www.development + '/',
-    testing : self.endpoints.www.production + '/',
-    staging : self.endpoints.www.production + '/',
+    testing : self.endpoints.www.testing + '/',
+    staging : self.endpoints.www.staging + '/',
     production : self.endpoints.www.production + '/'
   };
 
