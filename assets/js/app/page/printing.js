1b4d68948e945b66315339ef8ba1c3f1606976a6
Stephanie Grieger <stefanie.grieger@coureon.com>
2016-06-13 09:32:57 +0200

    COUREON-1919 integrate rates in frontend

--- assets/js/app/page/printing.js
+++ assets/js/app/page/printing.js
@@ -136,213 +136,216 @@ function(
     }
   };
 
+  self.originalRates = function() {
+    var rates = StorageShipment.rates;
+    return rates;
+  };
 
-
-  self.originalRates = [
-    {
-      'code': 'dpag_national',
-      'carrier_code': 'dpag',
-      'gross_amount': 4.8,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 4,
-      'surcharges': [
-        {
-          'code': '19:dpag_einschreiben',
-          'name': 'dpag_einschreiben',
-          'amount': 2.5
-        },
-        {
-          'code': '20:dpag_einschreiben_einwurf',
-          'name': 'dpag_einschreiben_einwurf',
-          'amount': 2.15
-        },
-        {
-          'code': '21:dpag_einschreiben_eigenhaendig',
-          'name': 'dpag_einschreiben_eigenhaendig',
-          'amount': 4.65
-        }
-      ]
-    },
-    {
-      'code': 'dpd_national_pickup',
-      'carrier_code': 'dpd',
-      'gross_amount': 3.99,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 2,
-      'surcharges': [
-        {
-          'code': '-1:DemoHoeherversicherung_2500EUR',
-          'name': 'DemoHoeherversicherung_2500EUR',
-          'amount': 3.75
-        },
-        {
-          'code': '24:dpd_predict',
-          'name': 'dpd_predict',
-          'amount': 0
-        },
-        {
-          'code': '25:dpd_tyre',
-          'name': 'dpd_tyre',
-          'amount': 5
-        },
-        {
-          'code': '26:dpd_tyre_predict',
-          'name': 'dpd_tyre_predict',
-          'amount': 5
-        }
-      ]
-    },
-    {
-      'code': 'dpd_national_dropoff',
-      'carrier_code': 'dpd',
-      'gross_amount': 3.99,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 2,
-      'surcharges': [
-        {
-          'code': '3:dpd_tyre',
-          'name': 'dpd_tyre',
-          'amount': 3.75
-        },
-        {
-          'code': '4:dpd_tyre_predict',
-          'name': 'dpd_tyre_predict',
-          'amount': 3.75
-        }
-      ]
-    },
-    {
-      'code': 'ccl_national_pickup',
-      'carrier_code': 'cclogistics',
-      'gross_amount': 28.02,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 1,
-      'surcharges': []
-    },
-    {
-      'code': 'ccl_national_priority_pickup',
-      'carrier_code': 'cclogistics',
-      'gross_amount': 29.57,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 1,
-      'surcharges': [
-        {
-          'code': '8:ccl_time_8',
-          'name': 'ccl_time_8',
-          'amount': 15
-        },
-        {
-          'code': '9:ccl_time_9',
-          'name': 'ccl_time_9',
-          'amount': 9
-        },
-        {
-          'code': '10:ccl_saturday',
-          'name': 'ccl_saturday',
-          'amount': 12.5
-        }
-      ]
-    },
-    {
-      'code': 'gel_national_pickup',
-      'carrier_code': 'gelexpress',
-      'gross_amount': 33.5,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 1,
-      'surcharges': [
-        {
-          'code': '27:gel_time_10',
-          'name': 'gel_time_10',
-          'amount': 28
-        },
-        {
-          'code': '28:gel_time_12',
-          'name': 'gel_time_12',
-          'amount': 16
-        },
-        {
-          'code': '29:gel_saturday_10',
-          'name': 'gel_saturday_10',
-          'amount': 49
-        },
-        {
-          'code': '30:gel_saturday_12',
-          'name': 'gel_saturday_12',
-          'amount': 28
-        },
-        {
-          'code': '31:gel_afternoon',
-          'name': 'gel_afternoon',
-          'amount': 8
-        }
-      ]
-    },
-    {
-      'code': 'gel_national_xxl_pickup',
-      'carrier_code': 'gelexpress',
-      'gross_amount': 81.10000000000001,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 1,
-      'surcharges': []
-    },
-    {
-      'code': 'gel_national_pallet_pickup',
-      'carrier_code': 'gelexpress',
-      'gross_amount': 98.35000000000001,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 1,
-      'surcharges': []
-    },
-    {
-      'code': 'hermes_national_pickup',
-      'carrier_code': 'hermes',
-      'gross_amount': 4.11,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 2,
-      'surcharges': [
-        {
-          'code': '33:hermes_pickup',
-          'name': 'hermes_pickup',
-          'amount': 3
-        }
-      ]
-    },
-    {
-      'code': 'myhermes_national',
-      'carrier_code': 'hermes',
-      'gross_amount': 4.58,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 2,
-      'surcharges': []
-    },
-    {
-      'code': 'ups_standard_dropoff',
-      'carrier_code': 'ups',
-      'gross_amount': 8.75,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 3,
-      'surcharges': []
-    },
-    {
-      'code': 'ups_standard_pickup',
-      'carrier_code': 'ups',
-      'gross_amount': 8.75,
-      'currency': 'EUR',
-      'days_min': 1,
-      'days_max': 3,
-      'surcharges': []
-    }
-  ];
+  // self.originalRates = [
+  //   {
+  //     'code': 'dpag_national',
+  //     'carrier_code': 'dpag',
+  //     'gross_amount': 4.8,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 4,
+  //     'surcharges': [
+  //       {
+  //         'code': '19:dpag_einschreiben',
+  //         'name': 'dpag_einschreiben',
+  //         'amount': 2.5
+  //       },
+  //       {
+  //         'code': '20:dpag_einschreiben_einwurf',
+  //         'name': 'dpag_einschreiben_einwurf',
+  //         'amount': 2.15
+  //       },
+  //       {
+  //         'code': '21:dpag_einschreiben_eigenhaendig',
+  //         'name': 'dpag_einschreiben_eigenhaendig',
+  //         'amount': 4.65
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'code': 'dpd_national_pickup',
+  //     'carrier_code': 'dpd',
+  //     'gross_amount': 3.99,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 2,
+  //     'surcharges': [
+  //       {
+  //         'code': '-1:DemoHoeherversicherung_2500EUR',
+  //         'name': 'DemoHoeherversicherung_2500EUR',
+  //         'amount': 3.75
+  //       },
+  //       {
+  //         'code': '24:dpd_predict',
+  //         'name': 'dpd_predict',
+  //         'amount': 0
+  //       },
+  //       {
+  //         'code': '25:dpd_tyre',
+  //         'name': 'dpd_tyre',
+  //         'amount': 5
+  //       },
+  //       {
+  //         'code': '26:dpd_tyre_predict',
+  //         'name': 'dpd_tyre_predict',
+  //         'amount': 5
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'code': 'dpd_national_dropoff',
+  //     'carrier_code': 'dpd',
+  //     'gross_amount': 3.99,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 2,
+  //     'surcharges': [
+  //       {
+  //         'code': '3:dpd_tyre',
+  //         'name': 'dpd_tyre',
+  //         'amount': 3.75
+  //       },
+  //       {
+  //         'code': '4:dpd_tyre_predict',
+  //         'name': 'dpd_tyre_predict',
+  //         'amount': 3.75
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'code': 'ccl_national_pickup',
+  //     'carrier_code': 'cclogistics',
+  //     'gross_amount': 28.02,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 1,
+  //     'surcharges': []
+  //   },
+  //   {
+  //     'code': 'ccl_national_priority_pickup',
+  //     'carrier_code': 'cclogistics',
+  //     'gross_amount': 29.57,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 1,
+  //     'surcharges': [
+  //       {
+  //         'code': '8:ccl_time_8',
+  //         'name': 'ccl_time_8',
+  //         'amount': 15
+  //       },
+  //       {
+  //         'code': '9:ccl_time_9',
+  //         'name': 'ccl_time_9',
+  //         'amount': 9
+  //       },
+  //       {
+  //         'code': '10:ccl_saturday',
+  //         'name': 'ccl_saturday',
+  //         'amount': 12.5
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'code': 'gel_national_pickup',
+  //     'carrier_code': 'gelexpress',
+  //     'gross_amount': 33.5,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 1,
+  //     'surcharges': [
+  //       {
+  //         'code': '27:gel_time_10',
+  //         'name': 'gel_time_10',
+  //         'amount': 28
+  //       },
+  //       {
+  //         'code': '28:gel_time_12',
+  //         'name': 'gel_time_12',
+  //         'amount': 16
+  //       },
+  //       {
+  //         'code': '29:gel_saturday_10',
+  //         'name': 'gel_saturday_10',
+  //         'amount': 49
+  //       },
+  //       {
+  //         'code': '30:gel_saturday_12',
+  //         'name': 'gel_saturday_12',
+  //         'amount': 28
+  //       },
+  //       {
+  //         'code': '31:gel_afternoon',
+  //         'name': 'gel_afternoon',
+  //         'amount': 8
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'code': 'gel_national_xxl_pickup',
+  //     'carrier_code': 'gelexpress',
+  //     'gross_amount': 81.10000000000001,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 1,
+  //     'surcharges': []
+  //   },
+  //   {
+  //     'code': 'gel_national_pallet_pickup',
+  //     'carrier_code': 'gelexpress',
+  //     'gross_amount': 98.35000000000001,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 1,
+  //     'surcharges': []
+  //   },
+  //   {
+  //     'code': 'hermes_national_pickup',
+  //     'carrier_code': 'hermes',
+  //     'gross_amount': 4.11,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 2,
+  //     'surcharges': [
+  //       {
+  //         'code': '33:hermes_pickup',
+  //         'name': 'hermes_pickup',
+  //         'amount': 3
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'code': 'myhermes_national',
+  //     'carrier_code': 'hermes',
+  //     'gross_amount': 4.58,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 2,
+  //     'surcharges': []
+  //   },
+  //   {
+  //     'code': 'ups_standard_dropoff',
+  //     'carrier_code': 'ups',
+  //     'gross_amount': 8.75,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 3,
+  //     'surcharges': []
+  //   },
+  //   {
+  //     'code': 'ups_standard_pickup',
+  //     'carrier_code': 'ups',
+  //     'gross_amount': 8.75,
+  //     'currency': 'EUR',
+  //     'days_min': 1,
+  //     'days_max': 3,
+  //     'surcharges': []
+  //   }
+  // ];
 
 }];
--- assets/js/app/storage/shipment.js
+++ assets/js/app/storage/shipment.js
@@ -11,17 +11,13 @@ module.exports = [
     self.rates = null;
 
     self.load = function(trackingId) {
-      console.log(trackingId);
-
       CommonRequest.shipment.get({
         trackingNumber: trackingId
       }, function(response) {
         if (response && response.content) {
-          console.log(response.content);
-
-
           setAddresses(response.content.result.address_from, response.content.result.address_to);
           self.rates = response.content.result.rates;
+          console.log('rates');
           console.log(self.rates);
         }
       });
