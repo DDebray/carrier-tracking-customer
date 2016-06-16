2a8991c1534ad370e1dae059f4ce4a973fd1e33d
Stephanie Grieger <stefanie.grieger@coureon.com>
2016-06-16 15:23:57 +0200

    COUREON-1786 Finish choose pament process.

--- assets/js/app/page/printing.js
+++ assets/js/app/page/printing.js
@@ -1,55 +1,113 @@
 module.exports = [
-  '$routeParams', 'CommonBrowser', 'StorageShipment', 'StorageTransaction',
-function(
-  $routeParams, CommonBrowser, StorageShipment, StorageTransaction
-) {
-  'use strict';
-  var self = this;
+  '$routeParams', 'CommonUi', 'StorageShipment', 'StorageTransaction',
+  function(
+    $routeParams, CommonUi, StorageShipment, StorageTransaction
+  ) {
+    'use strict';
+    var self = this;
 
-  self.currentAddress = false;
-
-  CommonBrowser.enableElementQueries();
-
-  if ($routeParams.trackingId) {
-    StorageShipment.load($routeParams.trackingId);
-  }
-
-  self.rates = function() {
-    return StorageShipment.rates;
-  };
-
-  self.addresses = function() {
-    return StorageShipment.addresses;
-  };
-
-  self.editAddress = function(address, addressType) {
-    self.currentAddress = address;
-  };
-
-  var update = function() {
-    StorageShipment.update(self.currentAddress);
     self.currentAddress = false;
-  };
 
-  self.formConfig = {
-    submit : {
-      label : 'COMMON.ADDRESSES.SAVE',
-      action : update
-    },
-    model : function() {
-      return self.currentAddress;
-    },
-    flags : {
-      enhanceFields : true
+    if ($routeParams.trackingId) {
+      StorageShipment.load($routeParams.trackingId);
     }
-  };
 
-  self.partialSearch = function () { return true; };
-  self.selectRate = function () { return true; };
-  self.calculatePickupDate = function () { return true; };
-  self.utcOffset = function () { return true; };
-  self.getSelectedSurcharge = function () { return true; };
+    self.rates = function() {
+      return StorageShipment.rates;
+    };
 
-  StorageTransaction.openPaymentMethodModal();
+    self.addresses = function() {
+      return StorageShipment.addresses;
+    };
 
-}];
+    self.editAddress = function(address, addressType) {
+      self.currentAddress = address;
+    };
+
+    var update = function() {
+      StorageShipment.update(self.currentAddress);
+      self.currentAddress = false;
+    };
+
+    self.formConfig = {
+      submit: {
+        label: 'COMMON.ADDRESSES.SAVE',
+        action: update
+      },
+      model: function() {
+        return self.currentAddress;
+      },
+      flags: {
+        enhanceFields: true
+      }
+    };
+
+    self.partialSearch = function() {
+      return true;
+    };
+    self.selectRate = function() {
+      return true;
+    };
+    self.calculatePickupDate = function() {
+      return true;
+    };
+    self.utcOffset = function() {
+      return true;
+    };
+    self.getSelectedSurcharge = function() {
+      return true;
+    };
+
+
+
+    // MODAL CONFIGURATION
+
+    // DEBUGGING
+    console.log('%c' + '\t\t\t\t\t\tStorage\t\t\t\t\t\tModal', 'font-weight:bold');
+
+    self.openPaymentMethodModal = function() {
+      var url = 'views/partials/modals/payment_methods.html';
+
+      var selectPaymentMethod = function(paymentMethod) {
+        CommonUi.modal.data.selected = paymentMethod;
+        console.log('[selected method] \t\t' + StorageTransaction.selectedPaymentMethod + '\t\t\t\t\t\t' + CommonUi.modal.data.selected);
+      };
+
+      var cleanPaymentDataInput = function(property) {
+        if (!CommonUi.modal.data.account || !CommonUi.modal.data.account[property]) {
+          return;
+        }
+        if (CommonUi.modal.data.account[property].search(/\W/) !== -1) {
+          CommonUi.modal.data.account[property] = CommonUi.modal.data.account[property].replace(/\W/g, '');
+        }
+        console.log('[account ' + property + '] \t\t\t' + StorageTransaction.paymentMethods.SOFORT_UEBERWEISUNG.data + '\t\t\t\t\t\t' + CommonUi.modal.data.account[property]);
+      };
+
+      var submitSelection = function() {
+        StorageTransaction.selectedPaymentMethod = CommonUi.modal.data.selected;
+        console.log('%c' + '--- Submit -----------------------------------------------------------------', 'font-weight:bold');
+        console.log('[selected method] \t\t' + StorageTransaction.selectedPaymentMethod + '\t\t\t' + CommonUi.modal.data.selected);
+        if (CommonUi.modal.data.account !== null) {
+          StorageTransaction.paymentMethods[StorageTransaction.selectedPaymentMethod].data = {};
+          StorageTransaction.paymentMethods[StorageTransaction.selectedPaymentMethod].data.iban = CommonUi.modal.data.account.iban;
+          StorageTransaction.paymentMethods[StorageTransaction.selectedPaymentMethod].data.bic = CommonUi.modal.data.account.bic;
+          console.log('[account iban] \t\t\t' + StorageTransaction.paymentMethods.SOFORT_UEBERWEISUNG.data.iban + '\t\t' + CommonUi.modal.data.account.iban);
+          console.log('[account bic] \t\t\t' + StorageTransaction.paymentMethods.SOFORT_UEBERWEISUNG.data.bic + '\t\t\t\t\t' + CommonUi.modal.data.account.bic);
+        }
+      };
+
+      CommonUi.modal.show(url, true, {
+        methods: Object.keys(StorageTransaction.paymentMethods),
+        selected: 'PAYPAL',
+        account: null
+      }, null, {
+        selectMethod: selectPaymentMethod,
+        cleanData: cleanPaymentDataInput,
+        submit: submitSelection
+      });
+    };
+
+    self.openPaymentMethodModal();
+
+  }
+];
--- assets/js/app/storage/transaction.js
+++ assets/js/app/storage/transaction.js
@@ -1,61 +1,23 @@
 module.exports = [
-  'CommonUi', 'CommonRequest',
+  'CommonRequest',
   function(
-    CommonUi, CommonRequest
+    CommonRequest
   ) {
     'use strict';
+
     var self = this;
 
-    self.availablePaymentMethods = {
-      PAYPAL : {
-        icon : 'fa-paypal',
-        data : {}
+    self.selectedPaymentMethod = null;
+    self.paymentMethods = {
+      CREDIT_CARD: {
+        data: null
       },
-      SOFORT_UEBERWEISUNG : {
-        icon : 'fa-envelope',
-        data : {}
+      PAYPAL: {
+        data: null
+      },
+      SOFORT_UEBERWEISUNG: {
+        data: null
       }
-    };
-    self.selectedPaymentMethod = 'PAYPAL';
-
-
-    self.openPaymentMethodModal = function() {
-      var url = 'views/partials/modals/payment_methods.html';
-
-      var selectPaymentMethod = function(paymentMethod) {
-        CommonUi.modal.data.selectedMethod = paymentMethod;
-        console.log('Modal: ', CommonUi.modal.data.selectedMethod);
-        console.log('Storage: ', self.selectedPaymentMethod);
-      };
-
-      var submitPaymentMethodSelection = function() {
-        self.selectedPaymentMethod = CommonUi.modal.data.selectedMethod;
-        console.log('Transaction Storage: ', self.selectedPaymentMethod);
-      };
-
-      var changePaymentData = function(paymentMethod) {
-        console.log('Modal: ', CommonUi.modal.data.paymentData);
-        console.log('Storage: ', self.availablePaymentMethods[paymentMethod].data);
-        console.log(CommonUi.modal.data);
-      };
-
-      CommonUi.modal.show(url, true, {
-        availableMethods : self.availablePaymentMethods,
-        selectedMethod : self.selectedPaymentMethod,
-        paymentData : {}
-      }, null, {
-        selectMethod: selectPaymentMethod,
-        submitSelection : submitPaymentMethodSelection,
-        changeData : changePaymentData
-      });
-    };
-
-    self.openPrintModal = function() {
-      var url = 'views/partials/';
-    };
-
-    self.openApprovalModal = function() {
-      var url = 'views/partials/';
     };
 
     return self;
