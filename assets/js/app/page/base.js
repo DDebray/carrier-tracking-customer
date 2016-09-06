9c3ad26ee1695499d9ebe1916e26478b294b3c2e
Lukas Magedanz <lukas.magedanz@gmail.com>
2016-09-06 21:16:28 +0200

    COUREON-2221, refactoring

--- assets/js/app/page/base.js
+++ assets/js/app/page/base.js
@@ -1,13 +1,34 @@
 module.exports = [
-  '$rootScope', '$location', '$route', '$translate', '$filter', 'CommonUi', 'CommonConfig', 'CommonTracking',
+  '$rootScope', '$location', '$route', '$translate', '$filter', 'CommonUi', 'CommonConfig', 'CommonTracking', 'StorageService',
 function(
-  $rootScope, $location, $route, $translate, $filter, CommonUi, CommonConfig, CommonTracking
+  $rootScope, $location, $route, $translate, $filter, CommonUi, CommonConfig, CommonTracking, StorageService
 ) {
   'use strict';
   var self = this;
 
   self.viewLocked = function() {
     return !!CommonUi.modal.template;
+  };
+
+  self.selectedLanguage = CommonConfig.selectedLanguage;
+
+  self.supportedLanguages = ['de'];
+  $translate(['COMMON.LANGUAGE.SUPPORTED_LANGUAGES']).then(function(translations) {
+    self.supportedLanguages = JSON.parse(translations['COMMON.LANGUAGE.SUPPORTED_LANGUAGES']);
+  });
+
+  self.changeLanguage = function (key) {
+    var previousLanguage = self.selectedLanguage,
+      languageNotification = $filter('translate')('COMMON.LANGUAGE.LANGUAGE_CHANGED')
+        .replace('%OLD%', $filter('translate')('COMMON.LANGUAGE.' + previousLanguage.toUpperCase()))
+        .replace('%NEW%', $filter('translate')('COMMON.LANGUAGE.' + key.toUpperCase()));
+
+    $translate.use(key).then(function (key) {
+      self.selectedLanguage = key;
+
+      CommonConfig.selectedLanguage = self.selectedLanguage;
+      StorageService.set('selectedLanguage', self.selectedLanguage);
+    });
   };
 
   CommonTracking.init();
