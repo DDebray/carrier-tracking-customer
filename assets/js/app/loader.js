6f04deb8a70a465b3f72182a400808916b058c50
Stephanie Grieger <stefanie.grieger@coureon.com>
2016-11-07 14:33:37 +0100

    format route, loader and ui/

--- assets/js/app/loader.js
+++ assets/js/app/loader.js
@@ -1,36 +1,36 @@
-var angular = require('angular');
-require('angular-resource'); // $resource
-require('angular-route'); // $route
-require('angular-translate'); // $translate
-require('angular-hotkeys'); // hotkeys
+var angular = require( 'angular' );
+require( 'angular-resource' ); // $resource
+require( 'angular-route' ); // $route
+require( 'angular-translate' ); // $translate
+require( 'angular-hotkeys' ); // hotkeys
 
-var application = angular.module('application', ['ngResource', 'ngRoute', 'pascalprecht.translate', 'cfp.hotkeys']);
-require('./common/filters')(application);
-require('./common/directives')(application);
+var application = angular.module( 'application', [ 'ngResource', 'ngRoute', 'pascalprecht.translate', 'cfp.hotkeys' ] );
+require( './common/filters' )( application );
+require( './common/directives' )( application );
 
 application
-  .provider('CommonConfig', require('./common/config'))
+  .provider( 'CommonConfig', require( './common/config' ) )
 
-  .config(require('./route'))
+.config( require( './route' ) )
 
-  .factory('$translateUrlLoader', require('./translation/service'))
-  .factory('CommonRequest', require('./common/request'))
-  .factory('CommonBrowser', require('./common/browser'))
-  .factory('CommonMoment', require('./common/moment'))
-  .factory('CommonTracking', require('./common/tracking'))
-  .factory('CommonPopups', require('./common/popups'))
+.factory( '$translateUrlLoader', require( './translation/service' ) )
+  .factory( 'CommonRequest', require( './common/request' ) )
+  .factory( 'CommonBrowser', require( './common/browser' ) )
+  .factory( 'CommonMoment', require( './common/moment' ) )
+  .factory( 'CommonTracking', require( './common/tracking' ) )
+  .factory( 'CommonPopups', require( './common/popups' ) )
 
-  .factory('StorageService', require('./storage/service'))
-  .factory('StorageTracking', require('./storage/tracking'))
-  .factory('StorageShipment', require('./storage/shipment'))
-  .factory('StorageTransaction', require('./storage/transaction'))
-  .factory('StorageCountries', require('./storage/countries'))
+.factory( 'StorageService', require( './storage/service' ) )
+  .factory( 'StorageTracking', require( './storage/tracking' ) )
+  .factory( 'StorageShipment', require( './storage/shipment' ) )
+  .factory( 'StorageTransaction', require( './storage/transaction' ) )
+  .factory( 'StorageCountries', require( './storage/countries' ) )
 
-  .provider('CommonUi', require('./ui/config'))
+.provider( 'CommonUi', require( './ui/config' ) )
 
-  .controller('UiCtrl', require('./ui/controller'))
-  .controller('PageBaseCtrl', require('./page/base'))
-  .controller('PageTrackingCtrl', require('./page/tracking'))
-  .controller('PagePrintingCtrl', require('./page/printing'));
+.controller( 'UiCtrl', require( './ui/controller' ) )
+  .controller( 'PageBaseCtrl', require( './page/base' ) )
+  .controller( 'PageTrackingCtrl', require( './page/tracking' ) )
+  .controller( 'PagePrintingCtrl', require( './page/printing' ) );
 
 module.exports = application;
