a707434685f81eb0b30c41b461fbac6fcbbd6a74
Stephanie Grieger <stefanie.grieger@coureon.com>
2016-11-07 14:37:55 +0100

    format js/storage/

--- assets/js/app/storage/service.js
+++ assets/js/app/storage/service.js
@@ -1,19 +1,19 @@
-module.exports = [function() {
+module.exports = [ function () {
   'use strict';
 
   var self = {
-    get : function(key) {
-      var data = localStorage.getItem('main');
-      data = data ? JSON.parse(data) : {};
-      return key ? data[key] : data;
+    get: function ( key ) {
+      var data = localStorage.getItem( 'main' );
+      data = data ? JSON.parse( data ) : {};
+      return key ? data[ key ] : data;
     },
-    set : function(key, value) {
+    set: function ( key, value ) {
       var data = self.get();
-      data[key] = value;
-      localStorage.setItem('main', JSON.stringify(data));
+      data[ key ] = value;
+      localStorage.setItem( 'main', JSON.stringify( data ) );
       return data;
     }
   };
 
   return self;
-}];
+} ];
