d22fa12b34d64a38ce04716deb91ed4b915d11c6
Stephanie Grieger <stefanie.grieger@coureon.com>
2016-11-23 15:41:01 +0100

    COUREON-2404 change logic for carrier information to differ between gls de, fr and es. Show carrier information for last event

--- assets/js/app/page/tracking.js
+++ assets/js/app/page/tracking.js
@@ -103,34 +103,15 @@ module.exports = [
       }
     } ];
 
-    var getCarrierInfoByEvents = function ( events ) {
-      // used to test multiple carriers
-      // events.push({
-      //   carrier: {
-      //     code: 'hermes'
-      //   }
-      // });
-      // events.push({
-      //   carrier: {
-      //     code: 'gls'
-      //   }
-      // });
+    var getCarrierInfo = function ( events, routes ) {
+      var lastEvent = events[ events.length - 1 ];
+      var carrier = lastEvent.carrier;
 
-      var uniqueBy = function ( a, key ) {
-        var seen = {};
-        return a.filter( function ( item ) {
-          var k = key( item );
-          return seen.hasOwnProperty( k ) ? false : ( seen[ k ] = true );
-        } );
-      };
+      if ( carrier.code === 'gls' ) {
+        carrier.country = routes[ lastEvent.route_number - 1 ].country;
+      }
 
-      // create an array that only contains all the carriers from the events
-      var unfilteredCarriers = events.map( function ( event ) {
-        return event.carrier;
-      } );
-
-      // filter dublicates
-      return uniqueBy( unfilteredCarriers, JSON.stringify );
+      return carrier;
     };
 
     // get trackingId from URL
@@ -145,10 +126,10 @@ module.exports = [
 
           self.showError = response.status === 'NOT_AVAILABLE';
 
-          if ( response && response.events && !!response.events.length ) {
+          if ( response && response.events && !!response.events.length && response.route_information && !!response.route_information.length ) {
             self.state = self.availableStates.indexOf( response.status );
             self.errorState = self.availableErrorStates.indexOf( response.status );
-            self.carrierInfo = getCarrierInfoByEvents( response.events );
+            self.carrierInfo = getCarrierInfo( response.events, response.route_information );
           } else {
             self.showError = true;
           }
