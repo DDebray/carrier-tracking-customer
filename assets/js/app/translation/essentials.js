f582d1b2c01a139c471b206098c10bb064e80a71
alex-coureon <alejandro.pajaron.jorquera@coureon.com>
2017-08-30 10:22:10 +0200

    COOP-138 - replace phone number for support email

--- assets/js/app/translation/essentials.js
+++ assets/js/app/translation/essentials.js
@@ -3,6 +3,6 @@ module.exports = {
     'PAGE.MAINTENANCE.MODAL.ERROR': 'Wartungsarbeiten',
     'PAGE.MAINTENANCE.MODAL.ERROR.TEXT': 'Entschuldigung, wir führen leider gerade Wartungsarbeiten durch. Wir bitten um ein wenig Geduld, unser System steht Ihnen in wenigen Minuten wieder zur Verfügung. Sobald dies der Fall ist, schließt sich dieses Fenster automatisch.',
     'UI.ACTIONS.HOTLINE': 'Fragen? %%',
-    'COMMON.HOTLINE': '030 / 208 499 84'
+    'COMMON.HOTLINE': 'support@coureon.com'
   }
 };
