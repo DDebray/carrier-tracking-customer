cbd3085c0ea05bb6e6202213ec5df45a6e351297
Jonathan Reisdorf <jonathan.reisdorf@project-a.com>
2018-02-13 17:01:50 +0100

    return label: create return label, add modals for confirmation and download, copy styles over from FE repo, refine translations, add fake address for initial rates display

--- assets/js/app/page/return.js
+++ assets/js/app/page/return.js
@@ -1,9 +1,17 @@
-module.exports = ['$routeParams', 'CommonRequest',
-  function($routeParams, CommonRequest) {
+module.exports = ['$routeParams', '$timeout', 'CommonRequest', 'CommonUi',
+  function($routeParams, $timeout, CommonRequest, CommonUi) {
     const authorization = $routeParams.authorization;
     this.shipment = null;
     this.errors = false;
     this.isEditingAddress = true;
+    const fakeAddress = {
+      name: 'Max Mustermann',
+      street1: 'Alexanderplatz',
+      street_no: '1',
+      postal_code: '10178',
+      city: 'Berlin',
+      country: 'DE'
+    };
     this.senderAddress = {
       country: 'DE'
     };
@@ -16,20 +24,22 @@ module.exports = ['$routeParams', 'CommonRequest',
       mass_unit: 'kg'
     };
 
-    const getAssembledShipment = () => {
+    const getAssembledShipment = (rateCode, senderAddress) => {
       return Object.assign({}, {
         packages: [this.package],
-        address_from: this.senderAddress
+        address_from: senderAddress || this.senderAddress,
+        selected_rate_code: rateCode
       });
     };
 
     const disassembleShipment = shipment => {
-      this.senderAddress = Object.assign({}, shipment && shipment.address_from || this.senderAddress);
+      const addressFrom = shipment && shipment.address_from;
+      this.senderAddress = Object.assign({}, addressFrom && addressFrom.name !== fakeAddress.name && addressFrom || this.senderAddress);
       this.package = Object.assign({}, shipment && shipment.packages && shipment.packages[0] || this.package);
     };
 
     this.onUpdate = response => {
-      console.log(response);
+      console.log('onUpdate', response); // DEBUG
 
       if (!response || !response.status) {
         // TODO show generic error
@@ -46,7 +56,10 @@ module.exports = ['$routeParams', 'CommonRequest',
     };
 
     CommonRequest.setToken(authorization);
-    CommonRequest.return.shipment.get({}).$promise.then(this.onUpdate).catch(this.onUpdate);
+    CommonRequest.return.shipment.get({}).$promise
+      .then(this.onUpdate)
+      .catch(this.onUpdate)
+      .then(() => this.updateRates(fakeAddress));
 
     this.addressForm = {
       model: () => this.senderAddress,
@@ -54,20 +67,65 @@ module.exports = ['$routeParams', 'CommonRequest',
         enhanceFields: true
       },
       submit: {
-        label: 'COMMON.ADDRESSES.SAVE',
+        label: 'PAGE.RETURN.SENDER_ADDRESS.SAVE',
         action: () => {
+          this.addressForm.submit.label = 'PAGE.RETURN.SENDER_ADDRESS.UPDATE';
           this.updateRates();
           this.isEditingAddress = false;
         }
       }
     };
 
-    this.updateRates = () => {
-      return CommonRequest.return.calculateRates.save({}, {shipment: getAssembledShipment()}).$promise.then(this.onUpdate).catch(this.onUpdate);
+    this.updateRates = address => {
+      return CommonRequest.return.calculateRates.save({}, { shipment: getAssembledShipment(null, address) }).$promise.then(this.onUpdate).catch(this.onUpdate);
     };
 
     this.selectRate = rate => {
-      console.log(rate);
+      CommonUi.modal.show( 'views/partials/modals/return/confirm.html', true, {
+        rate,
+        shipment: this.shipment,
+        isBusy: false
+      }, null, {
+        confirm: modal => {
+          modal.closable = false;
+          modal.data.isBusy = true;
+          CommonRequest.return.shipment.create({}, { shipment: getAssembledShipment(rate.code) })
+            .$promise.then(this.onLabelCreationStarted).catch(this.onLabelCreationStarted);
+        }
+      });
+    };
+
+    this.onLabelCreationStarted = response => {
+      if (response && (response.status !== 'OK' || typeof response.status === 'number')) {
+        this.errors = response.messages;
+        return CommonUi.modal.close();
+      }
+
+      if (!response || !response.status || !response.content || !response.content.task_id) {
+        // TODO show generic error
+        return CommonUi.modal.close();
+      }
+
+      CommonUi.modal.show( 'views/partials/modals/return/download.html', false, {
+        isBusy: true
+      }, null, {
+        check: taskId => {
+          CommonRequest.return.task.get({ taskId }).$promise
+            .then(response => this.onLabelCreationPending(taskId, response))
+            .catch(error => this.onLabelCreationPending(taskId, error));
+        }
+      });
+
+      this.onLabelCreationPending(response.content.task_id);
+    };
+
+    this.onLabelCreationPending = (taskId, response) => {
+      if (!response || response.content && response.content.progress && response.content.progress.running) {
+        return $timeout(() => CommonUi.modal.action.check(taskId), response && 3000 || 0);
+      }
+
+      CommonUi.modal.data.isBusy = false;
+      CommonUi.modal.data.downloads = response.content && response.content.download_result && response.content.download_result.download_urls;
     }
   }
 ];
